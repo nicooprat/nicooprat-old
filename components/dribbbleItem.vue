8d95e3cf396f58ffe063fe901a7c02d5cc386d9f
Nico Prat <nicooprat@gmail.com>
2018-04-28 21:17:51 +0200

    Dribbble

---
+++ components/dribbbleItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <a :href="shot.html_url">
+    <span>
+      <img :src="shot.images.hidpi || shot.images.normal" :alt="shot.title" width="400" height="300">
+    </span>
+    <h4 v-text="shot.title"></h4>
+    <small v-text="shot.description.replace(/(<([^>]+)>)/ig, ' ')"></small>
+    <i v-for="tag in shot.tags" :key="tag">#{{tag}}</i>
+  </a>
+</template>
+
+<script>
+  export default {
+    props: {
+      shot: {
+        title: {type: String, required: true},
+        thumb: {type: String, required: true},
+        excerpt: {type: String, required: false},
+        link: {type: String, required: true},
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~/assets/common.scss";
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    border-bottom: none;
+  }
+
+  span {
+    display: block;
+    position: relative;
+    padding-bottom: 75%;
+    background-color: rgba(black,.03);
+    margin-bottom: calc(var(--gutter) / 2);
+    overflow: hidden;
+  }
+
+  img {
+    display: block;
+    position: absolute;
+    top: 0; left: 0;
+    width: 100%; height: 100%;
+    object-fit: cover;
+    overflow: hidden;
+  }
+
+  h4 {
+    margin: 0;
+  }
+
+  small {
+    display: block;
+    margin-top: calc(var(--gutter) / 2);
+  }
+
+  i {
+    display: inline-block;
+    font-size: .8em;
+    opacity: .5;
+
+    & + & {
+      margin-left: .5em;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
     <codepen :pens="codepen"/>
     <twitter :tweets="twitter"/>
     <meetup :events="meetup"/>
+    <dribbble :shots="dribbble"/>
 
     <form name="contact" method="POST" netlify>
       <p>
@@ -33,6 +34,7 @@
   import codepen from '~/components/codepen.vue'
   import twitter from '~/components/twitter.vue'
   import meetup from '~/components/meetup.vue'
+  import dribbble from '~/components/dribbble.vue'
 
   export default {
     components: {
@@ -43,6 +45,7 @@
       twitter,
       twitter,
       meetup,
+      dribbble,
     },
 
     data() {
@@ -51,6 +54,7 @@
         codepen: [],
         twitter: [],
         meetup: [],
+        dribbble: [],
       }
     },
 
@@ -59,11 +63,13 @@
       const codepen = await axios.get('https://api.apify.com/v1/KJmGFZ2mADwTHyKpp/crawlers/gDEqWbPh4p97ukpEH/lastExec/results?token=oAHr2Bn6Hf73u7b2H2HjgWfct')
       const twitter = await axios.get('https://api.apify.com/v1/KJmGFZ2mADwTHyKpp/crawlers/zGiqmuLEFgDLC8diQ/lastExec/results?token=TdPModM6r6EyB4BG89wcsXqww')
       const meetup = await axios.get('https://api.meetup.com/self/events?desc=true&scroll=next_upcoming&photo-host=secure&page=12&sig_id=144086432&sig=53af04b00dfc30e042a19bb595be7e054ff71487')
+      const dribbble = await axios.get('https://api.dribbble.com/v2/user/shots?access_token=adf54688450e377e645e62c86e051ad245d211d71803266d099c54d4594a2234')
       return {
         medium: medium.data && medium.data[0].pageFunctionResult,
         codepen: codepen.data && codepen.data[0].pageFunctionResult,
         twitter: twitter.data && twitter.data[0].pageFunctionResult,
         meetup: meetup.data && meetup.data.slice(0,6),
+        dribbble: dribbble.data && dribbble.data.slice(0,6),
       }
     },
   }
