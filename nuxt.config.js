aa8d7f036a459823d3462c140ee9fc944efce27a
Nico Prat <nicooprat@gmail.com>
2018-05-09 13:41:37 +0200

    Favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,16 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Designer & développeur front-end' }
+      { hid: 'description', name: 'description', content: 'Designer & développeur front-end' },
+      { name: 'msapplication-TileColor', content: '#4c91dd' },
+      { name: 'theme-color', content: '#ffffff' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel:'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel:'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel:'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel:'manifest', href: '/site.webmanifest' },
+      { rel:'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Arvo:400,700|Roboto:400,700,900' },
     ],
   },
