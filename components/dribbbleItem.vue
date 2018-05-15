390d8ba7be138ee75009df8ad365a3b23d1f0012
Nico Prat <nicooprat@gmail.com>
2018-05-15 18:11:11 +0200

    Lazy load Dribbble gifs

--- components/dribbbleItem.vue
+++ components/dribbbleItem.vue
@@ -1,7 +1,12 @@
 <template>
-  <a :href="html_url">
+  <a :href="html_url" @mouseenter="mouseEnter" @touchstart="mouseEnter" @mouseleave="mouseLeave" @touchend="mouseLeave" :data-state="state">
     <span>
-      <img :src="lazySrc" :data-src="src" :alt="title" width="400" height="300">
+      <img :src="src" :data-src="lazySrc" :alt="title" width="400" height="300">
+      <button v-if="state !== 'loaded' && isGif">
+        <strong v-if="state === 'standby' || state === 'loaded' && !hovering">&nbsp;▶︎</strong>
+        <strong v-if="state === 'loading'">∙∙∙</strong>
+      </button>
+      <img class="gif" v-if="state !== 'standby' && isGif && hovering" :src="images.hidpi || images.normal" :alt="title" width="400" height="300" @load="gifLoaded">
     </span>
     <h4 v-text="title"></h4>
     <small v-text="description.replace(/(<([^>]+)>)/ig, ' ')"></small>
@@ -20,32 +25,47 @@
     },
 
     data() {
+      const isGif = this.images.normal.includes('.gif')
       return {
-        lazySrc: !process.browser, // Only load if SSR
-        src: this.images.hidpi || this.images.normal
+        isGif,
+        lazySrc: isGif ? this.images.teaser : this.images.hidpi,
+        src: false,
+        state: isGif ? 'standby' : 'loaded',
+        hovering: false
       }
     },
 
     mounted() {
-      // Lazy load tweets
+      // Lazy load dribbbles
       if( typeof(IntersectionObserver) != 'undefined' ) {
         const component = this
         new IntersectionObserver(function(entries) {
           if(entries[0].isIntersecting) {
-            component.loadImg()
+            component.loadSrc()
             this.disconnect()
           }
         }, {
           rootMargin: '50%'
         }).observe(this.$el)
       } else {
-        setTimeout(() => this.loadImg(), 2000)
+        setTimeout(() => this.loadSrc(), 2000)
       }
     },
 
     methods: {
-      loadImg() {
-        this.lazySrc = this.src
+      loadSrc() {
+        this.src = this.lazySrc
+      },
+      mouseEnter(e) {
+        this.state = 'loading'
+        this.hovering = true
+      },
+      mouseLeave(e) {
+        this.state = 'standby'
+        this.hovering = false
+      },
+      gifLoaded(e) {
+        this.state = 'loaded'
       }
     },
   }
@@ -70,23 +90,67 @@
     display: block;
     position: relative;
     padding-bottom: 75%;
-    background-color: rgba(black,.03);
+    background: center no-repeat rgba(black,.03);
+    background-size: cover;
     margin-bottom: calc(var(--gutter) / 2);
     overflow: hidden;
-    transform: translate3d(0,0,20px);
+    transform: translateZ(0px);
+    transition: transform 200ms;
+    perspective: 100px;
   }
 
   img {
-    display: block;
     position: absolute;
     top: 0; left: 0;
     width: 100%; height: 100%;
     object-fit: cover;
     overflow: hidden;
 
+    // Lazyload
     &:not([src]) {
       visibility: hidden;
     }
+
+    &.gif {
+      display: none;
+    }
+  }
+
+  a[data-state="loading"] {
+
+    button {
+      animation: loading .8s infinite alternate ease-in-out;
+
+      @keyframes loading {
+        to {
+          transform: translate3d(-50%,-50%,-30px);
+        }
+      }
+    }
+  }
+
+  a[data-state="loaded"] {
+
+    .gif {
+      display: block;
+    }
+  }
+
+  button {
+    appearance: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 50%; left: 50%;
+    width: 2.5em; height: 2.5em;
+    background: white;
+    padding: 0;
+    border: none;
+    font: inherit;
+    color: $color;
+    border-radius: 999px;
+    transform: translate(-50%,-50%);
   }
 
   h4 {
