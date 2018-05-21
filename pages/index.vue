38030cede3e6426937d6b8a5625b5be05ac25b09
Nico Prat <nicooprat@gmail.com>
2018-05-21 15:21:29 +0200

    Fetch and parse calendar

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,7 @@
   import cachios from 'cachios'
   import anime from 'animejs'
   import animate from 'animate'
+  import ical from 'ical.js'
 
   import cover from '~/components/cover.vue'
   import intro from '~/components/intro.vue'
@@ -153,6 +154,18 @@
       const meetupPast = await cachios.get('https://api.meetup.com/self/events?desc=true&scroll=future_or_past&photo-host=public&page=6&sig_id=144086432&status=past&fields=group&sig=23e9ab019654e79ba238dc9e3a32de09cc4bac47', {ttl})
       const dribbble = await cachios.get('https://api.dribbble.com/v2/user/shots?access_token=adf54688450e377e645e62c86e051ad245d211d71803266d099c54d4594a2234', {ttl})
       const github = await cachios.get('https://api.apify.com/v1/KJmGFZ2mADwTHyKpp/crawlers/iMsuiLC6pCB3QFg6r/lastExec/results?token=Fo9YSdwmYwkY5e69ATkmpotJy', {ttl})
+      const calendar = await cachios.get('https://p59-calendars.icloud.com/published/2/gdFv46MDKOWq_7KQklX9VYNvGShbqD8yrSf0q-DqWV0_vJjFe7rnr-pXARuixyzzGKITVuBaXLyiY2vcbZKNeuRqFTIEaR8Be3meAJ-8P4M', {ttl})
+
+      // Parse events from ICS to JSON
+      // https://github.com/mozilla-comm/ical.js/issues/222#issuecomment-204083519
+      const comp = new ical.Component(ical.parse(calendar.data))
+      const events = comp.getAllSubcomponents('vevent').map(e => {
+        return {
+          name: e.getFirstPropertyValue('summary'),
+          start: e.getFirstPropertyValue('dtstart').toJSDate(),
+          end: e.getFirstPropertyValue('dtend').toJSDate(),
+        }
+      })
 
       return {
         medium: medium.data && medium.data[0].pageFunctionResult.slice(0,6),
@@ -161,6 +174,7 @@
         meetup: [...meetupUpcoming.data, ...meetupPast.data].slice(0,6),
         dribbble: dribbble.data && dribbble.data.slice(0,6),
         github: github.data && github.data[0].pageFunctionResult.slice(0,6),
+        events,
       }
     },
   }
