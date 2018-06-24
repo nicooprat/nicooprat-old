fd4df9d16e8459744be58609118b5439ce75e910
Nico Prat <nicooprat@gmail.com>
2018-06-24 13:07:05 +0200

    Revert "Use img proxy for Google Analytics"
    This reverts commit 97848890d90c8050b0a52d4e1a6f8711d41bd214.

--- pages/index.vue
+++ pages/index.vue
@@ -26,8 +26,6 @@
     <svg class="loader" width="64px" height="64px" viewBox="0 0 128 128">
       <path d="M75.4 126.63a11.43 11.43 0 0 1-2.1-22.65 40.9 40.9 0 0 0 30.5-30.6 11.4 11.4 0 1 1 22.27 4.87h.02a63.77 63.77 0 0 1-47.8 48.05v-.02a11.38 11.38 0 0 1-2.93.37z" fill="#ffffff" fill-opacity="1" />
     </svg>
-
-    <img src="https://ga-beacon.appspot.com/UA-25610541-1?useReferer&pixel" width="0" height="0" alt="None" aria-hidden="true" style="visibility: hidden;">
   </div>
 </template>
 
